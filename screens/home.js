621f57171673e1ead15879a86e8446a46b81f26c
PamelaMBadermann <badermann.pamela@gmail.com>
2026-06-23 12:13:49 -0300

    feat: adiciona Animated API

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,46 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { View, Text, Button, Animated } from 'react-native';
 import styles from '../styles/styles';
+
+const FadeInText = (props) => {
+	const fadeAnim = useRef(new Animated.Value(0)).current
+
+	React.useEffect(() => {
+	Animated.timing(
+		fadeAnim,
+		{
+		toValue: 1,
+		duration: 5000,
+		useNativeDriver: false,
+		}
+	).start();
+	}, [fadeAnim])
+
+	return (
+	<Animated.Text
+		style={{
+		fontSize: 20,
+		textAlign: 'center',
+		justifyContent: 'center',
+		margin: 10,
+		width: 250,
+		backgroundColor: '#007bff',
+		opacity: fadeAnim,
+		}}
+	>
+		{props.children}
+	</Animated.Text>
+	);
+}
 
 function HomeScreen({navigation}) {
 	return (
 		<View style={styles.container}>
 			<Text>Home Screen</Text>
+
+			<FadeInText>
+				Fade In Text
+			</FadeInText>
 		</View>
 	);
 }
--- styles/styles.js
+++ styles/styles.js
@@ -4,8 +4,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    justifyContent: 'center',
+    color: '007bff'
+  },
 });
 
 export default styles;
