bf78d822dc2413aa6ea9466959afe8f21c07d2c6
PamelaMBadermann <badermann.pamela@gmail.com>
2026-06-23 12:42:50 -0300

    feat: adiciona LayoutAnimation API

--- screens/about.js
+++ screens/about.js
@@ -1,13 +1,75 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import styles from '../styles/styles';
+import React, { useState } from 'react';
+import {
+  UIManager,
+  LayoutAnimation,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
 
-function AboutScreen({navigation}) {
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
+function AboutScreen() {
+  const [size, setSize] = useState({
+    w: 200,
+    h: 200,
+  });
+
+  const onPress = () => {
+    LayoutAnimation.spring();
+
+    setSize({
+      w: size.w - 5,
+      h: size.h - 5,
+    });
+  };
+
   return (
-		<View style={styles.container}>
-			<Text>About Screen</Text>
-		</View>
-	);
+    <View style={styles.container}>
+      <Text>About Screen</Text>
+
+      <View
+        style={[
+          styles.box,
+          {
+            width: size.w,
+            height: size.h,
+          },
+        ]}
+      />
+
+      <TouchableOpacity onPress={onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>
+            Click to decrease the size of the square.
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    backgroundColor: 'orange',
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
 
 export default AboutScreen;
